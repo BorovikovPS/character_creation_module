a1341fadeb1cfef39af8ef4f5019955aae3b5db5
Pavel Borovikov <pavel-borovikv@yandex.ru>
2023-01-20 12:23:31 +0300

    Code is formatted PEP8

--- main.py
+++ main.py
@@ -1,52 +1,52 @@
 from random import randint
 
 
-def attack(char_name, char_class):
+def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
-        attack_warrior = 5 + randint(3, 5)
+        attack_warrior: float = 5 + randint(3, 5)
         return (f'{char_name} нанёс урон противнику равный '
                 f'{attack_warrior}')
     if char_class == 'mage':
-        attack_mage = 5 + randint(5, 10)
+        attack_mage: float = 5 + randint(5, 10)
         return (f'{char_name} нанёс урон противнику равный '
                 f'{attack_mage}')
     if char_class == 'healer':
-        attack_healer = 5 + randint(-3, -1)
+        attack_healer: float = 5 + randint(-3, -1)
         return (f'{char_name} нанёс урон противнику равный '
                 f'{attack_healer}')
 
 
-def defence(char_name, char_class):
+def defence(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
-        defence_warrior = 10 + randint(5, 10)
+        defence_warrior: float = 10 + randint(5, 10)
         return (f'{char_name} блокировал'
                 f'{defence_warrior} урона')
     if char_class == 'mage':
-        defence_mage = 10 + randint(-2, 2)
+        defence_mage: float = 10 + randint(-2, 2)
         return (f'{char_name} блокировал'
                 f'{defence_mage} урона')
     if char_class == 'healer':
-        defence_healer = 10 + randint(2, 5)
+        defence_healer: float = 10 + randint(2, 5)
         return (f'{char_name} блокировал'
                 f'{defence_healer} урона')
 
 
-def special(char_name, char_class):
+def special(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
-        special_warrior = 80 + 25
+        special_warrior: float = 80 + 25
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {special_warrior}»')
     if char_class == 'mage':
-        special_warrior = 5 + 40
+        special_warrior: float = 5 + 40
         return (f'{char_name} применил специальное умение '
                 f'«Атака {special_warrior}»')
     if char_class == 'healer':
-        special_healer = 10 + 30
+        special_healer: float = 10 + 30
         return (f'{char_name} применил специальное умение '
                 f'«Защита {special_healer}»')
 
 
-def start_training(char_name, char_class):
+def start_training(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -61,7 +61,7 @@ def start_training(char_name, char_class):
     print('Если не хочешь тренироваться, введи команду skip.')
     cmd = None
     while cmd != 'skip':
-        cmd = input('Введи команду: ')
+        cmd: str = input('Введи команду: ')
         if cmd == 'attack':
             print(attack(char_name, char_class))
         if cmd == 'defence':
@@ -71,15 +71,19 @@ def start_training(char_name, char_class):
     return 'Тренировка окончена.'
 
 
-def choice_char_class():
-    approve_choice = None
-    char_class = None
+def choice_char_class() -> str:
+    approve_choice: str = None
+    if approve_choice is None:
+        return approve_choice
+    char_class: str = None
+    if char_class is None:
+        return char_class
     while approve_choice != 'y':
-        char_class = input('Введи название персонажа, '
-                           'за которого хочешь играть: '
-                           'Воитель — warrior, '
-                           'Маг — mage, '
-                           'Лекарь — healer:')
+        char_class: str = input('Введи название персонажа, '
+                                'за которого хочешь играть: '
+                                'Воитель — warrior, '
+                                'Маг — mage, '
+                                'Лекарь — healer:')
         if char_class == 'warrior':
             print('Воитель — дерзкий воин ближнего боя. '
                   'Сильный, выносливый и отважный.')
@@ -89,22 +93,65 @@ def choice_char_class():
         if char_class == 'healer':
             print('Лекарь — могущественный заклинатель. '
                   'Черпает силы из природы, веры и духов. ')
-        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, '
-                               'или любую другую кнопку, чтобы выбрать '
-                               'другого персонажа.').lower()
+        approve_choice: str = input('Нажми (Y), чтобы подтвердить выбор, '
+                                    'или любую другую кнопку, чтобы выбрать '
+                                    'другого персонажа.').lower()
     return char_class
 
 
-def main():
+def main() -> str:
     print('Приветствую тебя, искатель приключений! ')
     print('Прежде чем начать игру... ')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы: ')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
 
 main()
+
+
+# Тестовые данные.
+# TEST_DATA: list[tuple[int, str, bool]] = [
+#     (44, 'success', True),
+#     (16, 'failure', True),
+#     (4, 'success', False),
+#     (21, 'failure', False),
+# ]
+
+# BONUS: float = 1.1
+# ANTIBONUS: float = 0.8
+
+
+# def add_rep(current_rep: float, rep_points: int, buf_effect: bool) -> float:
+#     current_rep += rep_points
+#     if buf_effect:
+#         return current_rep * BONUS
+#     return current_rep
+
+
+# def remove_rep(
+#         current_rep: float, rep_points: int, debuf_effect: bool
+# ) -> float:
+#     current_rep -= rep_points
+#     if debuf_effect:
+#         return current_rep * ANTIBONUS
+#     return current_rep
+
+
+# def main(duel_res: str):
+#     current_rep = 0.0
+#     for rep, result, effect in duel_res:
+#         if result == 'success':
+#             current_rep = add_rep(current_rep, rep, effect)
+#         if result == 'failure':
+#             current_rep = remove_rep(current_rep, rep, effect)
+#     return (f'После {len(duel_res)} поединков, ',
+#             'репутация персонажа — {current_rep:.3f} очков.')
+
+
+# # Тестовый вызов функции main.
+# print(main(TEST_DATA))
